9205eea4e1f05632973f929a69d31640fb799b88
Olasupo Olawale Martin <rytesoft@hotmail.com>
2023-07-26 17:04:57 +0200

    second commit

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ counter = 0
 score_total = 0
 test_score = 0
 
-while test_score != 101:
+while test_score >= 0 and test_score <= 100:
     test_score = int(input("Enter test score: "))
-    if test_score >= 0 and test_score <= 100:
-        score_total += test_score
-        counter += 1
+    score_total += test_score
+    counter += 1
 
 average_score = round(score_total / counter)
 
