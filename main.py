b315141e0fa456c06d5cb218e83d92c850738bc9
Olasupo Olawale Martin <rytesoft@hotmail.com>
2023-08-31 15:07:41 +0200

    date of birth function 1.1

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import date_of_birth_calculation as p
 
 print("Voter registration")
+
 fullname = input("Enter full name:    ")
 
 birth_date = input("Please enter your date of birth (DD/MM/YYYY):   ")
 
 result = p.dob(birth_date)
-if result < 18 and result > 65:
+
+if result > 18 and result < 65:
     print(f"Your age is {result} years. {fullname}, you can now proceed to vote.")
 else:
     print(f"Your age is {result} years. {fullname}, you are not eligible to vote.")
